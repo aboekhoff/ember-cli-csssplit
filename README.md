5d947a305b33cd99a08cb9524e04435f6dc31f09
Andrew Boekhoff <andrew.boekhoff@kaybus.com>
2014-08-19 14:08:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Split Your CSS With Broccoli
+
+Pass your tree to broccoli-csssplit and it will split any
+file (eg. foo.css) with more than 4095 selectors into separate files.
---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var path  = require('path');
+var fs    = require('fs');
+var csss  = require('broccoli-csssplit');
+
+function CSSSPlugin(options) {
+  this.name = 'ember-cli-csssplit';
+  this.options = options || {};
+}
+
+CSSSPlugin.prototype.toTree = function(tree) {
+  return csss(tree, this.options);
+};
+
+function EmberCLICSSS(project) {
+  if (!(this instanceof EmberCLICSSS)) { return new EmberCLICSSS(project); }
+  this.project = project;
+  this.name    = 'Ember CLI CSSS';
+}
+
+EmberCLICSSS.prototype.treeFor = function treeFor() {
+};
+
+EmberCLICSSS.prototype.included = function included(app) {
+  var registry = app.registry;
+  this.app = app;
+
+  var plugin = new CSSSPlugin(this.app.options.csssOptions);
+
+  registry.add('css', plugin);
+};
+
+module.exports = EmberCLICSSS;
