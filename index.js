15fbea87e632c74c644f7a60c736b3adbdfaff19
Tim Stirrat <tim.stirrat@gmail.com>
2014-09-14 11:17:32 -0700

    Cleanup unused modules/code

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 'use strict';
 
-var path  = require('path');
-var fs    = require('fs');
 var csss  = require('broccoli-csssplit');
 var pickFiles  = require('broccoli-static-compiler');
-var mergeTrees  = require('broccoli-merge-trees');
 
 function EmberCLICSSS(project) {
   if (!(this instanceof EmberCLICSSS)) { return new EmberCLICSSS(project); }
   this.project = project;
   this.name    = 'ember-cli-csssplit';
 }
-
-EmberCLICSSS.prototype.treeFor = function treeFor() {
-};
 
 EmberCLICSSS.prototype.included = function included(app) {
   this.app     = app;
